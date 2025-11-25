d77e6e1d51308771ff3e116ea9bab3d005f48189
Irina Saf <irina.safronova0801@gmail.com>
2025-11-25 09:01:47 -0500

    Added Variables

--- app/src/main/java/com/example/tapcounter2025/MainActivity.kt
+++ app/src/main/java/com/example/tapcounter2025/MainActivity.kt
@@ -1,12 +1,44 @@
 package com.example.tapcounter2025
 
+import android.media.MediaActionSound
+import android.media.MediaPlayer
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.widget.Button
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var timerText: TextView
+    private lateinit var countText: TextView
+    private lateinit var topScoresText: TextView
+    private lateinit var tapButton: ImageButton
+    private lateinit var resetButton: Button
+    private lateinit var resetHighScoresButton: Button
+
+    private var tapCount = 0
+    private var isRunning = false
+    private lateinit var timer: CountDownTimer
+    private  val topScores = mutableListOf<Int>()
+
+    private lateinit var tapSound: MediaPlayer
+    private lateinit var gameOverSound: MediaPlayer
+
+    private val PREFS_NAME = "HighScores"
+    private val SCORES_KEY = "TopScores"
+
+    private var screenWidt = 0
+    private var screenHight = 0
+
+    private var originalX = 0f
+    private var originalY = 0f
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
