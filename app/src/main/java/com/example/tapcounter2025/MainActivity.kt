f7456ac2b47c91863a529526b76f30fc07a8d2fb
Irina Saf <irina.safronova0801@gmail.com>
2025-11-25 10:21:29 -0500

    Implemented Reset Button

--- app/src/main/java/com/example/tapcounter2025/MainActivity.kt
+++ app/src/main/java/com/example/tapcounter2025/MainActivity.kt
@@ -79,6 +79,18 @@ class MainActivity : AppCompatActivity() {
             countText.text = getString(R.string.taps, tapCount)
         }
 
+        resetButton.setOnClickListener {
+            if(isRunning)
+            {
+                timer.cancel()
+            }
+            tapCount = 0
+            countText.text = getString(R.string.taps_0)
+            timerText.text = getString(R.string.time_left_20)
+            tapButton.isEnabled = true
+            isRunning = false
+        }
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
