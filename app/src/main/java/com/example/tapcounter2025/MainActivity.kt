e922dacb610b094ae393e2897afdef04675d1be0
Irina Saf <irina.safronova0801@gmail.com>
2025-11-26 08:37:14 -0500

    Added High Scores Functions

--- app/src/main/java/com/example/tapcounter2025/MainActivity.kt
+++ app/src/main/java/com/example/tapcounter2025/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.tapcounter2025
 
+import android.content.Context
 import android.media.MediaPlayer
 import android.os.Bundle
 import android.os.CountDownTimer
@@ -62,7 +63,7 @@ class MainActivity : AppCompatActivity() {
             }
 
             override fun onFinish() {
-                timerText.text = getString(R.string.time_up)
+                timerText.text = getString(R.string.times_up)
                 tapButton.isEnabled = false
                 isRunning = false
             }
@@ -80,7 +81,7 @@ class MainActivity : AppCompatActivity() {
         }
 
         resetButton.setOnClickListener {
-            if(isRunning)
+            if (isRunning)
             {
                 timer.cancel()
             }
@@ -97,4 +98,52 @@ class MainActivity : AppCompatActivity() {
             insets
         }
     }
+
+    private  fun updateTopScores() {
+
+    topScores.add(tapCount)
+    topScores.sortDescending()
+
+    if (topScores.size > 5) {
+        topScores.removeAt(topScores.lastIndex)
+    }
+    saveTopScores()
+    displayTopScores()
+}
+
+    private  fun displayTopScores() {
+        val scoreText = StringBuilder(getString(R.string.top_5_scores)+"\n")
+        topScores.forEachIndexed{index, score ->
+            scoreText.append("${index +1}: $score\n")
+        }
+        topScoresText.text = scoreText.toString()
+    }
+
+    private  fun saveTopScores(){
+        val sharedPref = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.putString(SCORES_KEY, topScores.joinToString (","))
+        editor.apply()
+    }
+
+    private  fun loadTopScores(){
+        val sharedPref = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val savedScores = sharedPref.getString(SCORES_KEY, "")
+
+        if (!savedScores.isNullOrEmpty()) {
+            topScores.clear()
+            topScores.addAll(savedScores.split(",").map { it.toInt() })
+        }
+    }
+
+    private  fun clearHighScores(){
+        val sharedPref = getSharedPreferences(PREFS_NAME, Context.MODE_PRIVATE)
+        val editor = sharedPref.edit()
+        editor.remove(SCORES_KEY)
+        editor.apply()
+
+        topScores.clear()
+    }
+
+
 }
